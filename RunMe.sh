d84653b5fc57b9a2a61e06ba5fce7bb637b85b5c
andreas <andreas.makris@gmail.com>
2017-07-10 16:56:08 +0200

    Remove online stuff and NFZ (thx to err0r4o4). Also give options to keep social stuff or not.

--- RunMe.sh
+++ RunMe.sh
@@ -11,7 +11,11 @@ cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
 options=(1 "force FCC patch" on
          2 "remove forced Updates from DJI Go4" on
          3 "remove Firmware Upgrade check" off
-		 4 "offline login" off)
+		 4 "offline login (thx artu-ole)" off
+		 5 "remove Onlinefunction [only use with offline login!] (thx err0r4o4)" off
+		 6 "remove Google APIs (keep if you want to keep social)" off
+		 7 "remove social networks (keep Google APIs too!)" off
+		 8 "remove NFZ db (thx err0r4o4)" off)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
 for choice in $choices
@@ -37,6 +41,31 @@ do
 			patch -l -p1 < ../patches/offlineLogin.patch
 			cd ..
             ;;
+		5)
+            cd decompile_out
+			patch -l -p1 < ../patches/removeOnlinefunction.patch
+			cd ..
+            ;;	
+		6)
+            cd decompile_out
+			patch -l -p1 < ../patches/removeGoogleApis.patch
+			cd ..
+            ;;	
+		7)
+            cd decompile_out
+			patch -l -p1 < ../patches/removeSocial.patch
+			cd ..
+            ;;	
+		8)
+            cd decompile_out
+			rm assets/flysafe/flysafe_areas_djigo.db
+			rm assets/flysafe/flyforbid_airmap/*.json
+			rm res/raw/flyforbid.json
+			cp ../patches/nfz/flyforbid.json res/raw/flyforbid.json
+			cp ../patches/nfz/flyforbid_airmap/* assets/flysafe/flyforbid_airmap/
+			cp ../patches/nfz/flysafe_areas_djigo.db assets/flysafe/flysafe_areas_djigo.db
+			cd ..
+            ;;		
     esac
 done
 echo =======================
