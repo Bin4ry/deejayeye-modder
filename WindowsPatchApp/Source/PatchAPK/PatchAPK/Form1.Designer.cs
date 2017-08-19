21d0d2e4cacdaa57376b5f4fb95faae9925749f9
djayeyeballs <djayeyeballs@gmail.com>
2017-08-18 21:42:40 -0230

    Added PatchAPK source code

---
+++ WindowsPatchApp/Source/PatchAPK/PatchAPK/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace PatchAPK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDecompile = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.clbPatch = new System.Windows.Forms.CheckedListBox();
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.btnRecompile = new System.Windows.Forms.Button();
+            this.cbDryRun = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aPKVersionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblAPK = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDecompile
+            // 
+            this.btnDecompile.Location = new System.Drawing.Point(12, 399);
+            this.btnDecompile.Name = "btnDecompile";
+            this.btnDecompile.Size = new System.Drawing.Size(142, 40);
+            this.btnDecompile.TabIndex = 2;
+            this.btnDecompile.Text = "Decompile";
+            this.btnDecompile.UseVisualStyleBackColor = true;
+            this.btnDecompile.Click += new System.EventHandler(this.btnDecompile_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.SystemColors.Control;
+            this.txtOutput.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtOutput.Location = new System.Drawing.Point(551, 51);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(932, 689);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // clbPatch
+            // 
+            this.clbPatch.CheckOnClick = true;
+            this.clbPatch.FormattingEnabled = true;
+            this.clbPatch.Location = new System.Drawing.Point(12, 51);
+            this.clbPatch.Name = "clbPatch";
+            this.clbPatch.Size = new System.Drawing.Size(533, 342);
+            this.clbPatch.TabIndex = 12;
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Location = new System.Drawing.Point(160, 399);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(142, 40);
+            this.btnPatch.TabIndex = 13;
+            this.btnPatch.Text = "Patch";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // btnRecompile
+            // 
+            this.btnRecompile.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRecompile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecompile.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRecompile.Location = new System.Drawing.Point(12, 445);
+            this.btnRecompile.Name = "btnRecompile";
+            this.btnRecompile.Size = new System.Drawing.Size(290, 40);
+            this.btnRecompile.TabIndex = 14;
+            this.btnRecompile.Text = "Build and Sign";
+            this.btnRecompile.UseVisualStyleBackColor = true;
+            this.btnRecompile.Click += new System.EventHandler(this.btnRecompile_Click);
+            // 
+            // cbDryRun
+            // 
+            this.cbDryRun.AutoSize = true;
+            this.cbDryRun.Location = new System.Drawing.Point(308, 406);
+            this.cbDryRun.Name = "cbDryRun";
+            this.cbDryRun.Size = new System.Drawing.Size(116, 29);
+            this.cbDryRun.TabIndex = 15;
+            this.cbDryRun.Text = "Dry Run";
+            this.cbDryRun.UseVisualStyleBackColor = true;
+            this.cbDryRun.CheckedChanged += new System.EventHandler(this.cbDryRun_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1498, 40);
+            this.menuStrip1.TabIndex = 19;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.aPKVersionToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.menuToolStripMenuItem.Text = "File";
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(269, 38);
+            this.settingsToolStripMenuItem.Text = "Select APK";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // aPKVersionToolStripMenuItem
+            // 
+            this.aPKVersionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.aPKVersionToolStripMenuItem.Name = "aPKVersionToolStripMenuItem";
+            this.aPKVersionToolStripMenuItem.Size = new System.Drawing.Size(269, 38);
+            this.aPKVersionToolStripMenuItem.Text = "APK Version";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Checked = true;
+            this.toolStripMenuItem2.CheckOnClick = true;
+            this.toolStripMenuItem2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolStripMenuItem2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(164, 38);
+            this.toolStripMenuItem2.Text = "4.1.3";
+            this.toolStripMenuItem2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.CheckOnClick = true;
+            this.toolStripMenuItem3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(164, 38);
+            this.toolStripMenuItem3.Text = "4.1.4";
+            this.toolStripMenuItem3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(92, 36);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // lblAPK
+            // 
+            this.lblAPK.AutoSize = true;
+            this.lblAPK.Location = new System.Drawing.Point(12, 714);
+            this.lblAPK.Name = "lblAPK";
+            this.lblAPK.Size = new System.Drawing.Size(70, 25);
+            this.lblAPK.TabIndex = 20;
+            this.lblAPK.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1498, 752);
+            this.Controls.Add(this.lblAPK);
+            this.Controls.Add(this.cbDryRun);
+            this.Controls.Add(this.btnRecompile);
+            this.Controls.Add(this.btnPatch);
+            this.Controls.Add(this.clbPatch);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnDecompile);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WinAPK Patcher - DJayEyeBalls";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecompile;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.CheckedListBox clbPatch;
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.Button btnRecompile;
+        private System.Windows.Forms.CheckBox cbDryRun;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aPKVersionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label lblAPK;
+    }
+}
+
---
+++ WindowsPatchApp/Source/PatchAPK/PatchAPK/Form1.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatchAPK
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string apk = "";
+        public string toolsdir = "";
+        public string patchdir = "";
+        public string decompiledir = "";        
+
+        private bool IsValidPath(string path)
+        {
+            Regex driveCheck = new Regex(@"^[a-zA-Z]:\\$");
+            if (!driveCheck.IsMatch(path.Substring(0, 3)))
+            {
+                return false;
+            }
+
+            string strTheseAreInvalidFileNameChars = new string(Path.GetInvalidPathChars());
+            strTheseAreInvalidFileNameChars += @":/?*" + "\"";
+            Regex containsABadCharacter = new Regex("[" + Regex.Escape(strTheseAreInvalidFileNameChars) + "]");
+            
+            if (containsABadCharacter.IsMatch(path.Substring(3, path.Length - 3)))
+            {
+                return false;
+            }
+            
+            return true;
+        }        
+
+        private string getFileSize(String type, long val)
+        {
+            long b = val;
+            float kb = b / 1024;
+            float mb = kb / 1024;
+            float gb = mb / 1024;
+
+            switch (type)
+            {
+                case "kb":
+                    return kb.ToString("n2") + " kB";
+                case "mb":
+                    return mb.ToString("n2") + " MB";
+                case "gb":
+                    return gb.ToString("n2") + " GB";
+                default:
+                    return b.ToString() + " Bytes";
+            }
+        }
+
+        private void btnDecompile_Click(object sender, EventArgs e)
+        {
+            if (apk.Contains(".apk"))
+            {
+                Thread thread1 = new Thread(apktoolDecompile);
+                thread1.Start();
+            }
+            else
+            {
+                MessageBox.Show("No apk file selected to decompile!");
+            }
+
+        }
+
+        public void apktoolDecompile()
+        {
+            try
+            {                
+                Process proc = new Process();
+                proc.StartInfo.FileName = "java";
+                proc.StartInfo.Arguments = @"-jar " + toolsdir + "\\apktool.jar d -f -o " + decompiledir + " " + apk;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                SetTextBoxText("<-------------- Begin Decompile to " + decompiledir + " -------------->\r\n");
+                proc.Start();
+                proc.BeginOutputReadLine();
+                //proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                proc.Close();
+                SetTextBoxText("\r\n<-------------- Decompile Complete ----------->\r\n");               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void apktoolRecompile()
+        {
+            try
+            {                
+                Process proc = new Process();
+                proc.StartInfo.FileName = "java";
+                proc.StartInfo.Arguments = @"-jar " + toolsdir + "\\apktool.jar b -o " + decompiledir + "\\dist\\mod.apk " + decompiledir; 
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                SetTextBoxText("<-------------- Begin Recompile -------------->\r\n");
+                proc.Start();
+                proc.BeginOutputReadLine();
+                //proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                proc.Close();
+                SetTextBoxText("\r\n<-------------- Recompile Complete ----------->\r\n");
+
+                Thread thread2 = new Thread(apkSign);
+                thread2.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        const int MAX_PATH = 255;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+            [MarshalAs(UnmanagedType.LPTStr)]
+         string path,
+            [MarshalAs(UnmanagedType.LPTStr)]
+         StringBuilder shortPath,
+            int shortPathLength
+            );
+
+        private static string GetShortPath(string path)
+        {
+            var shortPath = new StringBuilder(MAX_PATH);
+            GetShortPathName(path, shortPath, MAX_PATH);
+            return shortPath.ToString();
+        }
+
+        public void apkSign()
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = "java";
+                proc.StartInfo.Arguments = @"-jar " + toolsdir + "\\sign.jar b " + decompiledir + "\\dist\\mod.apk --override";
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                SetTextBoxText("<-------------- Begin Sign APK -------------->\r\n");
+                proc.Start();
+                proc.BeginOutputReadLine();
+                //proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                proc.Close();
+                SetTextBoxText("\r\n<-------------- Sign APK Complete ----------->\r\n");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void bsPatch()
+        {
+            try
+            {                
+                Process proc = new Process();
+                proc.StartInfo.FileName = toolsdir + "\\bspatch";
+                proc.StartInfo.Arguments = decompiledir + "\\lib\\armeabi-v7a\\libSDKRelativeJNI.so " + decompiledir + "\\lib\\armeabi-v7a\\libSDKRelativeJNI.so " + patchdir + "\\so.bspatch";
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                SetTextBoxText("<-------------- Applying BSPATCH -------------->\r\n");
+                proc.Start();
+                proc.BeginOutputReadLine();
+                //proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                proc.Close();
+                SetTextBoxText("\r\n<-------------- BSPATCH Complete ----------->\r\n");                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void patch()
+        {
+            
+            for (int i = 0; i < clbPatch.Items.Count; i++) 
+            {
+                 string itm = clbPatch.Items[i].ToString();
+                 if(clbPatch.GetItemChecked(i) && itm != "removeOnlinefunction")
+                 {
+                     try
+                     {
+                         string preArg = "";
+                         Process proc = new Process();
+                         proc.StartInfo.FileName = toolsdir + "\\patch.exe";
+                         proc.StartInfo.WorkingDirectory = decompiledir;
+                         string f = patchdir + "\\" + clbPatch.Items[i].ToString() + ".patch";
+                         if (cbDryRun.Checked)
+                         {
+                             preArg = "--dry-run ";
+                         }
+                         proc.StartInfo.Arguments = preArg + "-l -p1 -N -i " + f;
+                         proc.StartInfo.CreateNoWindow = true;
+                         proc.StartInfo.RedirectStandardOutput = true;
+                         proc.StartInfo.RedirectStandardError = true;
+                         proc.StartInfo.UseShellExecute = false;
+                         proc.StartInfo.Verb = "runas";
+                         proc.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                         SetTextBoxText("<-------------- Begin Patch -------------->\r\n");
+                         SetTextBoxText("Applying Patch: " + clbPatch.Items[i].ToString() + "\r\n");
+                         //MessageBox.Show(f);
+                         proc.Start();
+                         proc.WaitForExit();
+                         string result = proc.StandardOutput.ReadToEnd();
+                         string error = proc.StandardError.ReadToEnd();
+                         SetTextBoxText("\r\nResult: " + result);
+                         if (error != "" && error != null)
+                         {
+                             SetTextBoxText("\r\nError: " + error);
+                         }
+                         proc.Close();
+                         SetTextBoxText("\r\n<-------------- Patch Complete ----------->\r\n");
+
+                     }
+                     catch (Exception ex)
+                     {
+                         MessageBox.Show(ex.ToString());
+                     }
+                 }
+                 else if (clbPatch.GetItemChecked(i) && itm == "removeOnlinefunction")
+                 {
+                     Thread thread3 = new Thread(bsPatch);
+                     thread3.Start();
+                 }
+
+            }           
+            
+        }
+                
+        private void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (outLine.Data != null)
+            {
+                SetTextBoxText(outLine.Data.ToString());
+            }
+            
+        }
+
+        private delegate void SetTextBoxTextInvoker(string text);
+        private void SetTextBoxText(string text)
+        {
+
+            if (this.txtOutput.InvokeRequired)
+            {
+                this.txtOutput.Invoke(new SetTextBoxTextInvoker(SetTextBoxText), text);
+            }
+            else
+            {
+                this.txtOutput.AppendText(text + "\r\n");
+            }
+        }             
+
+        private void SetVars()
+        {            
+            System.IO.Directory.CreateDirectory(Application.StartupPath + "\\decompile");
+            apk = Application.StartupPath + "\\SELECT APK";
+            lblAPK.Text = "";
+            toolsdir = Application.StartupPath + "\\tools";
+            patchdir = Application.StartupPath + "\\patches\\" + getAPKVersion();
+            decompiledir = Application.StartupPath + "\\decompile";
+
+        }      
+  
+        private string getAPKVersion()
+        {
+            string version = "";
+            if (toolStripMenuItem3.Checked)
+            {
+                version = "4.1.4";
+            }
+            else
+            {
+                version = "4.1.3";
+            }
+            return version;
+        }
+
+       
+
+        private void loadPatches()
+        {
+            clbPatch.Items.Clear();
+            DirectoryInfo d = new DirectoryInfo(Application.StartupPath + "\\patches\\" + getAPKVersion());
+            FileInfo[] Files = d.GetFiles("*.patch");
+            foreach (FileInfo file in Files)
+            {
+                clbPatch.Items.Add(file.Name.Replace(".patch", ""));
+            }          
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetVars();           
+            loadPatches();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            if (clbPatch.SelectedItems.Count > 0)
+            {
+                patch();
+            }
+            else
+            {
+                MessageBox.Show("No patches selected!");
+            }
+        }
+
+        private void btnRecompile_Click(object sender, EventArgs e)
+        {
+            if (apk.Contains(".apk"))
+            {
+                Thread thread1 = new Thread(apktoolRecompile);
+                thread1.Start();
+            }
+            else
+            {
+                MessageBox.Show("No apk file selected to recompile!");
+            }
+            
+        }
+
+        private void cbDryRun_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileDialog fd = new OpenFileDialog();
+            fd.RestoreDirectory = true;
+            DialogResult result = fd.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = fd.FileName;
+                try
+                {
+                    apk = file;
+                    lblAPK.Text = file;
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frmA = new AboutBox1();
+            frmA.ShowDialog();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            if (toolStripMenuItem3.Checked)
+            {
+                toolStripMenuItem3.Checked = false;
+            }
+            else
+            {
+                toolStripMenuItem2.Checked = true;
+            }
+            loadPatches();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (toolStripMenuItem2.Checked)
+            {
+                toolStripMenuItem2.Checked = false;
+            }
+            else
+            {
+                toolStripMenuItem3.Checked = true;
+            }
+            loadPatches();
+        }
+    }
+}
